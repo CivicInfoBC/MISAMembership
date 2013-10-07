c469aa6f68e8738586e9941277623754e6e2870d
Robert Leahy <rleahy@rleahy.ca>
2013-10-07 08:52:53 -0700

    Search Overhaul
    The search in the top-right is now always present, and uses the MISA BC WordPress site's search.
    
    Added a "Search" link to the sidebar for admins only, which takes you to the "keyword" controller.
    
    The "keyword" controller now has a front-end, which is simply a form which allows you to enter search terms.

--- controllers/keyword.php
+++ controllers/keyword.php
@@ -1,95 +1,97 @@
 <?php
 
 
-	//	Get the keywords
+	$template=new Template(WHERE_TEMPLATES);
 	
-	unset($keywords_str);
-	
-	if (is_post()) {
-	
-		if (!isset($_POST['keywords'])) error(HTTP_BAD_REQUEST);
-		
-		$keywords_str=$_POST['keywords'];
-	
-	} else if (!is_null($request->GetQueryString('keywords'))) {
-	
-		$keywords_str=$request->GetQueryString('keywords');
-	
-	}
 	
 	if (
-		!isset($keywords_str) ||
-		is_null($keywords_str) ||
-		(($keywords_str=MBString::Trim($keywords_str))==='')
-	) error(HTTP_BAD_REQUEST);
+		//	Only perform search if it's
+		//	a POST request
+		is_post() &&
+		//	There must be POST parameters
+		isset($_POST['keywords']) &&
+		//	The POST parameters must not
+		//	be empty
+		(($keywords_str=MBString::Trim($_POST['keywords']))!=='')
+	) {
+	
+		//	Perform search/display results
+		
+		$title='Search Results';
+		
+		//	Split up keywords
+		$keywords=preg_split('/\\s+/u',$keywords_str);
+		
+		//	Get organization results
+		
+		$query=Organization::GetKeywordQuery($keywords);
+		
+		//	Get count
+		$count=Organization::GetCount($query);
+		
+		//	Use shared paginated processing
+		require(WHERE_LOCAL_PHP_INCLUDES.'list_shared.php');
+		
+		$org_template=new Template(WHERE_TEMPLATES);
+		$org_template->page=$page_num;
+		$org_template->per_page=$num_per_page;
+		$org_template->set_size=$count;
+		$org_template->pages=$num_pages;
+		$org_template->results=Organization::GetPage(
+			$page_num,
+			$num_per_page,
+			'`name` ASC',
+			$query
+		);
+		$org_template->no_type_select=true;
+		$org_template->keywords=$keywords_str;
+		
+		//	Get user results
+		
+		$query=User::GetKeywordQuery($keywords);
+		
+		//	Get count
+		$count=User::GetCount($query);
+		
+		//	Use shared paginated processing
+		require(WHERE_LOCAL_PHP_INCLUDES.'list_shared.php');
+		
+		$user_template=new Template(WHERE_TEMPLATES);
+		$user_template=new Template(WHERE_TEMPLATES);
+		$user_template->page=$page_num;
+		$user_template->per_page=$num_per_page;
+		$user_template->set_size=$count;
+		$user_template->pages=$num_pages;
+		$user_template->results=User::GetPage(
+			$page_num,
+			$num_per_page,
+			'`last_name` ASC,`first_name` ASC',
+			$query
+		);
+		$user_template->no_type_select=true;
+		$user_template->keywords=$keywords_str;
+		
+		
+		//	Render
+		$template->org=$org_template;
+		$template->user=$user_template;
+		Render(
+			$template,
+			'keyword.phtml'
+		);
+	
+	} else {
+	
+		//	Display form
+		
+		$title='Search';
+		
+		Render(
+			$template,
+			'keyword_search.phtml'
+		);
+		
+	}
 	
 	
-	$title='Search Results';
-	
-	
-	//	Split up keywords
-	$keywords=preg_split('/\\s+/u',$keywords_str);
-	
-	
-	//	Get organization results
-	
-	$query=Organization::GetKeywordQuery($keywords);
-	
-	//	Get count
-	$count=Organization::GetCount($query);
-	
-	//	Use shared paginated processing
-	require(WHERE_LOCAL_PHP_INCLUDES.'list_shared.php');
-	
-	$org_template=new Template(WHERE_TEMPLATES);
-	$org_template->page=$page_num;
-	$org_template->per_page=$num_per_page;
-	$org_template->set_size=$count;
-	$org_template->pages=$num_pages;
-	$org_template->results=Organization::GetPage(
-		$page_num,
-		$num_per_page,
-		'`name` ASC',
-		$query
-	);
-	$org_template->no_type_select=true;
-	$org_template->keywords=$keywords_str;
-	
-	
-	//	Get user results
-	
-	$query=User::GetKeywordQuery($keywords);
-	
-	//	Get count
-	$count=User::GetCount($query);
-	
-	//	Use shared paginated processing
-	require(WHERE_LOCAL_PHP_INCLUDES.'list_shared.php');
-	
-	$user_template=new Template(WHERE_TEMPLATES);
-	$user_template=new Template(WHERE_TEMPLATES);
-	$user_template->page=$page_num;
-	$user_template->per_page=$num_per_page;
-	$user_template->set_size=$count;
-	$user_template->pages=$num_pages;
-	$user_template->results=User::GetPage(
-		$page_num,
-		$num_per_page,
-		'`last_name` ASC,`first_name` ASC',
-		$query
-	);
-	$user_template->no_type_select=true;
-	$user_template->keywords=$keywords_str;
-	
-	
-	//	Render
-	$template=new Template(WHERE_TEMPLATES);
-	$template->org=$org_template;
-	$template->user=$user_template;
-	Render(
-		$template,
-		'keyword.phtml'
-	);
-	
-
 ?>
