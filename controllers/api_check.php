6e44e431c9e683bbcefd27fa13c297b81b48f178
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2015-02-25 21:23:28 -0800

    Check API Addition
    The "check" API call may now be passed a domain, in which case it will attempt to determine if the organization associated with that domain is in good standing.
    
    Details of/documentation for this API call may be found in api_check.php.

--- controllers/api_check.php
+++ controllers/api_check.php
@@ -8,6 +8,13 @@
 	//		"email":	<E-Mail to check>
 	//	}
 	//
+	//	or
+	//
+	//	{
+	//		"action":	"check"
+	//		"domain":	<E-Mail or domain to check>
+	//	}
+	//
 	//	And the response shall be:
 	//
 	//	{
@@ -19,35 +26,84 @@
 	//		"reason":
 	//		"bypass":
 	//	}
-
-
-	//	Verify needed argument is present
-	if (!isset($api_request->email)) api_error(HTTP_BAD_REQUEST);
 	
 	
-	//	Determine whether specified user
-	//	exists or not
-	$exists=!is_null(
-		$user=User::GetByUsername($api_request->email)
-	);
-	
-	$api_result=array('exists' => $exists);
-	
-	if ($exists) {
+	function check ($user) {
 	
 		//	Determine whether specified user is
 		//	in good standing
 		$good_standing=$user->Check($code);
 		
-		$api_result['good_standing']=$good_standing;
+		$retr=array();
+		
+		$retr['good_standing']=$good_standing;
 		
 		if (!$good_standing) {
 		
-			$api_result['code']=$code;
-			$api_result['reason']=LoginAttempt::GetReason($code);
-			$api_result['bypass']=$user->type==='admin';
+			$retr['code']=$code;
+			$retr['reason']=LoginAttempt::GetReason($code);
+			$retr['bypass']=$user->type==='admin';
 		
 		}
+		
+		return $retr;
+	
+	}
+	
+	
+	if (isset($api_request->email)) {
+	
+		//	Check e-mail
+	
+		//	Determine whether specified user
+		//	exists or not
+		$exists=!is_null(
+			$user=User::GetByUsername($api_request->email)
+		);
+		
+		$api_result=array('exists' => $exists);
+		
+		if ($exists) $api_result=array_merge($api_result,check($user));
+	
+	} else if (isset($api_request->domain)) {
+	
+		//	Check domain
+		
+		//	The domain may just be an e-mail, so remove
+		//	the username and @ symbol
+		$domain=preg_replace('/^.*@/u','',$api_request->domain);
+		
+		$conn=$dependencies['MISADBConn'];
+		$query=sprintf(
+			'SELECT * FROM `users` WHERE `email` LIKE \'%%@%s\'',
+			preg_replace(
+				'(\\%|_)',
+				'\\$1',
+				$conn->real_escape_string($domain)
+			)
+		);
+		
+		if (($query=$conn->query($query))===false) throw new \Exception($conn->error);
+		
+		if ($query->num_rows===0) {
+		
+			$api_result=array('exists' => false);
+		
+		} else {
+		
+			//	TODO: Possibly loop
+		
+			$user=new User(new MySQLRow($query));
+			
+			$api_result=array_merge(array('exists' => true),check($user));
+		
+		}
+	
+	} else {
+	
+		//	Request is just bad
+	
+		api_error(HTTP_BAD_REQUEST);
 	
 	}
 
