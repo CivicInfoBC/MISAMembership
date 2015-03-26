aa1f7d996995e3550535d0ee8d70ad23a4ea618e
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2015-03-25 23:08:43 -0700

    Users Report
    Added the users report, as specified by Jason.

--- controllers/reports.php
+++ controllers/reports.php
@@ -108,6 +108,57 @@
 							payment in2
 							INNER JOIN membership_years in3 ON in2.membership_year_id=in3.id
 					) p ON ps.payment_id = p.payment_id'
+		),
+		'users' => (object)array(
+			'columns' => array(
+				'id' => 'ID',
+				'first_name' => 'First Name',
+				'last_name' => 'Last Name',
+				'email' => 'E-Mail',
+				'title' => 'Title',
+				'address' => 'Address',
+				'address2' => 'Address (Cont.)',
+				'city' => 'City',
+				'territorial_unit' => 'Province',
+				'country' => 'Country',
+				'phone' => 'Phone',
+				'cell' => 'Mobile',
+				'fax' => 'Fax',
+				'enabled' => 'Enabled?',
+				'type' => 'Type',
+				'opt_out' => 'Opt Out?',
+				'org_id' => 'Organization ID',
+				'org_name' => 'Organization Name',
+				'membership_type' => 'Membership Type',
+				'is_municipality' => 'Municipality?'
+			),
+			'query' =>
+				'SELECT
+					u.id,
+					u.first_name,
+					u.last_name,
+					u.email,
+					u.title,
+					u.address,
+					u.address2,
+					u.city,
+					u.territorial_unit,
+					u.postal_code,
+					u.country,
+					u.phone,
+					u.cell,
+					u.fax,
+					u.org_id,
+					IF(u.enabled,\'Yes\',\'No\') enabled,
+					u.type,
+					IF(u.opt_out,\'Yes\',\'No\') opt_out,
+					o.name org_name,
+					m.name membership_type,
+					IF(m.is_municipality,\'Yes\',\'No\') is_municipality
+				FROM
+					users u
+					LEFT JOIN organizations o ON u.org_id = o.id
+					LEFT JOIN membership_types m ON o.membership_type_id = m.id'
 		)
 	);
 	
